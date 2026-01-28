423e3010095a94e21c01174f2401205fd3a0173f
s43492 <s43492@bj.ac.th>
2026-01-28 10:27:14 +0700

    Add files via upload

---
+++ Final-เลขที่30-เลขที่33.capx/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1769571037,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/particles.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
