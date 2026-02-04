9f3904309d04e7c1fb3fc27b502dbafbabb3e7bd
s43492 <s43492@bj.ac.th>
2026-02-04 11:32:16 +0700

    Add files via upload

---
+++ a6_flappybird_เลขที่30/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1770179694,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/homebackground-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/downloadremovebgpreview-sheet0.png",
+		"images/imagesremovebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
